41ccd3b5273b6401ee8f2701bd9d3cd48e24f2f4
Oleg Baranov <baranov.off2@gmail.com>
2024-11-14 15:57:09 +0200

    add walls prototype

--- GameArcade/Scenes/FlappyBird/player_flappy.gd
+++ GameArcade/Scenes/FlappyBird/player_flappy.gd
@@ -9,9 +9,9 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+		
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept"):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -23,3 +23,11 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_area_2d_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	get_tree().quit()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	get_tree().quit()
